286c5dff8d181dba3e5c91b05153c36258aca895
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2024-05-26 23:31:17 +0800

    dist folder output

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // For Static Export
-  // output: 'export',
+  output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
   optimizeFonts: false,
+  distDir: "dist",
 };
 
 module.exports = nextConfig;
